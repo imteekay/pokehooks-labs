ec723b33485594b430165fa6aa13d45b37029f8b
= <leandrotk100@gmail.com>
2019-07-19 17:35:59 -0300

    Fetching pokemons from pokeapi

--- src/PokemonsList.js
+++ src/PokemonsList.js
@@ -1,15 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { PokemonContext } from './PokemonContext';
 
+const url = "https://pokeapi.co/api/v2/pokemon";
+
 const PokemonsList = () => {
-  const { state, capture } = useContext(PokemonContext);
+  const { state, capture, addPokemons } = useContext(PokemonContext);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      addPokemons(data.results);
+    };    
+
+    fetchPokemons();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="pokemons-list">
       <h2>Pokemons List</h2>
 
       {state.pokemons.map((pokemon) =>
-        <div key={`${pokemon.id}-${pokemon.name}`}>
+        <div key={pokemon.name}>
           <div>
             <span>{pokemon.name}</span>
             <button onClick={capture(pokemon)}>+</button>
