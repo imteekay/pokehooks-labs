a10918d54e8a24509f8a42d1155e67c04df5a308
TK <leandrotk100@gmail.com>
2020-04-01 22:17:33 -0300

    refactoring

--- src/Pokemon/PokemonContext.js
+++ src/Pokemon/PokemonContext.js
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react';
-import { usePokemonReducer } from './reducer';
+import { usePokemonReducer } from './usePokemonReducer';
 import { CAPTURE, RELEASE, ADD_POKEMON, ADD_POKEMONS } from './actions';
 
 const PokemonContext = createContext();
@@ -8,21 +8,10 @@ const PokemonProvider = (props) => {
   const [state, dispatch] = usePokemonReducer();
   const { pokemons, capturedPokemons } = state;
 
-  const capture = (pokemon) => () => {
-    dispatch({ type: CAPTURE, pokemon });
-  };
-
-  const release = (pokemon) => () => {
-    dispatch({ type: RELEASE, pokemon });
-  };
-
-  const addPokemon = (pokemon) => {
-    dispatch({ type: ADD_POKEMON, pokemon });
-  };
-
-  const addPokemons = (pokemons) => {
-    dispatch({ type: ADD_POKEMONS, pokemons });
-  };
+  const capture = (pokemon) => () => dispatch({ type: CAPTURE, pokemon });
+  const release = (pokemon) => () => dispatch({ type: RELEASE, pokemon });
+  const addPokemon = (pokemon) => dispatch({ type: ADD_POKEMON, pokemon });
+  const addPokemons = (pokemons) => dispatch({ type: ADD_POKEMONS, pokemons });
 
   const providerValue = {
     pokemons,
